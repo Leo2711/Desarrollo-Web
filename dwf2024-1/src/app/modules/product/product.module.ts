2de3bda572898f03815a05a32c6870286d369a80
Leo2711 <leonel2711@ciencias.unam.mx>
2023-10-08 19:22:51 -0600

    Practica 7, duda con el paso 5, al final lo colo que en category.component.ts, pero no estoy seguro, tengo duda en las partes del form en el modal, en donde coloque category id y form-control pero creo que esta bien

--- dwf2024-1/src/app/modules/product/product.module.ts
+++ dwf2024-1/src/app/modules/product/product.module.ts
@@ -6,6 +6,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './components/category/category.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
