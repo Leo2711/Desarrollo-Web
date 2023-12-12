98c39303704cd66f5e3bc4e4cb4057ea57d34d20
monica <monmm@ciencias.unam.mx>
2023-12-12 13:01:27 -0600

    Conexion carrito global y otras funciones

---
+++ dwf2024-1/src/app/modules/layout/_service/layout.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+  private layoutSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  public layout$: Observable<number> = this.layoutSubject.asObservable();
+
+  constructor() {}
+
+  updateLayout(newValue: number): void {
+    console.log(this.layout$);
+    this.layoutSubject.next(newValue);
+  }
+}
